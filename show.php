1af1bf001eeecf2c943fedad848274e8c73965b6
Prenapat <73680684+Prenapat@users.noreply.github.com>
2020-11-27 10:57:38 +0700

    Update show.php

--- show.php
+++ show.php
@@ -10,14 +10,14 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$res = mysqli_query($conn, 'SELECT * FROM Guestbook');
+$res = mysqli_query($conn, 'SELECT * FROM labb');
 ?>
 <table width="600" border="1">
   <tr>
-    <th width="100"> <div align="center">Product </div></th>
-    <th width="350"> <div align="center">Price </div></th>
+    <th width="150"> <div align="center">Product </div></th>
+    <th width="150"> <div align="center">Price </div></th>
     <th width="150"> <div align="center">Amount </div></th>
-    <th width="350"> <div align="center">Total </div></th>
+    <th width="150"> <div align="center">Total </div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
